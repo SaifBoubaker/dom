f80ec788194e1de3003a09348a4c26df0a9bd8a6
SaifBoubaker <106166944+SaifBoubaker@users.noreply.github.com>
2022-11-24 20:30:19 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var totalprice = document.getElementById("totalp");
+var sum = 0;
+// Plus Button
+function plusClick(id) {
+  console.log(id);
+  let quantity = document.getElementById("q" + id[1]);
+  quantity.innerHTML = parseInt(quantity.innerHTML) + 1;
+
+  let total = document.getElementById("t" + id[1]);
+  let price = document.getElementById("s" + id[1]);
+
+  total.innerHTML = parseInt(total.innerHTML) + parseInt(price.innerHTML);
+
+  sum = parseInt(price.innerHTML) + sum;
+  totalprice.innerHTML = sum;
+}
